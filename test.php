0a6f3db1ae9e3e3755888b18525f7f9544eba0ab
Wesley-4a5a <374053@edu.rocmn.nl>
2017-03-04 11:26:39 +0100

    testfiles

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'testclass.php';
+
+$classCall = new hondenJong('text1', 'text2', 'text3'); //de parameters neem je en assign je in __contruct.
+// $classCall->setVar();
+echo $classCall->testEcho('attribuuutz');
+echo $classCall->echoTwee();
+ ?>
---
+++ testclass.php
@@ -0,0 +1,35 @@
+<?php
+
+class hondenJong{
+
+  var $hond;
+  var $aap;
+  var $noot;
+
+  function __construct($num1, $num2, $num3){
+    // $this->hond = $num1;
+    // $this->aap = $num2;
+    // $this->noot = $num3;
+    $this->hond = $num3;
+  }
+
+  // function setVar(){
+  //   $this->hond = 'Static 1';
+  //   $this->aap = 'Static 2';
+  //   $this->noot = 'Static 3';
+  // }
+
+  function testEcho($ditisookmogelijk){
+    // echo $this->aap;
+    echo $this->hond; 
+    echo $ditisookmogelijk;  //dit werkt IN de functie zelf.
+    $this->parameterHandle = $ditisookmogelijk; //wil je de attribute meenemen? MOET $this->varnaam = parameter;
+  }
+
+  function echoTwee(){
+    echo '<br /><br />';
+    echo $this->attributeHandle;     //Dit is de parameter van testEcho;
+  }
+
+}
+ ?>
